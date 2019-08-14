682a1a456b023a28202b3c0c4e0e97bf84570880
Scott Dunkley <scottdunkley@gmail.com>
2019-08-14 23:48:55 +0100

    Basic stylecop setup with the copyright. Disabled some rules in stylecop. Moved MouseEventArgs to MouseEventArgs.cs as that's convention and was used as a test for stylecop to get right.

--- src/ClassicUO.Engine/Input/EventArgs.cs
+++ src/ClassicUO.Engine/Input/EventArgs.cs
@@ -1,10 +1,9 @@
-﻿#region license
-//  Copyright (C) 2019 ClassicUO Development Community on Github
+﻿// Copyright (C) 2019 ClassicUO Development Community on Github
 //
-//	This project is an alternative client for the game Ultima Online.
-//	The goal of this is to develop a lightweight client considering 
-//	new technologies.  
-//      
+// This project is an alternative client for the game Ultima Online.
+// The goal of this is to develop a lightweight client considering 
+// new technologies.
+//
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
@@ -17,35 +16,12 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#endregion
-using System;
-
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Input;
-
-using static SDL2.SDL;
-
 namespace ClassicUO.Input
 {
-    public sealed class MouseEventArgs : EventArgs
-    {
-        public MouseEventArgs(int x, int y, MouseButton button = MouseButton.None, ButtonState state = ButtonState.Released)
-        {
-            Location = new Point(x, y);
-            Button = button;
-            ButtonState = state;
-        }
-
-        public Point Location { get; }
-
-        public int X => Location.X;
-
-        public int Y => Location.Y;
-
-        public MouseButton Button { get; }
-
-        public ButtonState ButtonState { get; }
-    }
+    using System;
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Input;
+    using static SDL2.SDL;
 
     public sealed class MouseDoubleClickEventArgs : EventArgs
     {
