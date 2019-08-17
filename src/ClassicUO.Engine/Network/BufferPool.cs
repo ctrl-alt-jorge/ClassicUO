bfa8cc3745292873dddbe74d8d12bc9dd24b33c2
Scott Dunkley <scottdunkley@gmail.com>
2019-08-17 14:52:07 +0100

    Modified BufferPool.cs to follow coding standards.

--- src/ClassicUO.Engine/Network/BufferPool.cs
+++ src/ClassicUO.Engine/Network/BufferPool.cs
@@ -1,61 +1,73 @@
-﻿#region license
-
-//  Copyright (C) 2019 ClassicUO Development Community on Github
+﻿// Copyright (C) 2019 ClassicUO Development Community on Github.
 //
-//	This project is an alternative client for the game Ultima Online.
-//	The goal of this is to develop a lightweight client considering 
-//	new technologies.  
-//      
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
+// This project is an alternative client for the game Ultima Online.
+// The goal of this is to develop a lightweight client considering
+//  new technologies.
 //
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-#endregion
-
-using System.Collections.Generic;
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 namespace ClassicUO.Network
 {
+    using System.Collections.Generic;
+
     public class BufferPool
     {
-        private readonly int _arraySize;
-        private readonly int _capacity;
-        private readonly Queue<byte[]> _freeSegment;
+        private readonly int arraySize;
+        private readonly int capacity;
+        private readonly Queue<byte[]> freeSegment;
 
         public BufferPool(int capacity, int arraysize)
         {
-            _capacity = capacity;
-            _arraySize = arraysize;
-            _freeSegment = new Queue<byte[]>(capacity);
-            for (int i = 0; i < capacity; i++) _freeSegment.Enqueue(new byte[arraysize]);
+            this.capacity = capacity;
+            arraySize = arraysize;
+            freeSegment = new Queue<byte[]>(capacity);
+
+            for (int i = 0; i < capacity; i++)
+            {
+                freeSegment.Enqueue(new byte[arraysize]);
+            }
         }
 
         public byte[] GetFreeSegment()
         {
             lock (this)
             {
-                if (_freeSegment.Count > 0) return _freeSegment.Dequeue();
+                if (freeSegment.Count > 0)
+                {
+                    return freeSegment.Dequeue();
+                }
 
-                for (int i = 0; i < _capacity; i++) _freeSegment.Enqueue(new byte[_arraySize]);
+                for (int i = 0; i < capacity; i++)
+                {
+                    freeSegment.Enqueue(new byte[arraySize]);
+                }
 
-                return _freeSegment.Dequeue();
+                return freeSegment.Dequeue();
             }
         }
 
         public void AddFreeSegment(byte[] segment)
         {
-            if (segment == null) return;
+            if (segment == null)
+            {
+                return;
+            }
 
-            lock (this) _freeSegment.Enqueue(segment);
+            lock (this)
+            {
+                freeSegment.Enqueue(segment);
+            }
         }
     }
 }
