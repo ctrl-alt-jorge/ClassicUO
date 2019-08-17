941cd4fddc609a31db59cfc0bf49eb43e2bd1ef7
Scott Dunkley <scottdunkley@gmail.com>
2019-08-17 15:03:02 +0100

    Modified CircularBuffer.cs to conform to Stylecop rules.

--- src/ClassicUO.Engine/Network/CircularBuffer.cs
+++ src/ClassicUO.Engine/Network/CircularBuffer.cs
@@ -1,80 +1,65 @@
-﻿#region license
-
-//  Copyright (C) 2019 ClassicUO Development Community on Github
+﻿// Copyright (C) 2019 ClassicUO Development Community on Github.
 //
-//	This project is an alternative client for the game Ultima Online.
-//	The goal of this is to develop a lightweight client considering 
-//	new technologies.  
-//      
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
+// This project is an alternative client for the game Ultima Online.
+// The goal of this is to develop a lightweight client considering
+//  new technologies.
 //
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-#endregion
-
-using System;
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 namespace ClassicUO.Network
 {
-    internal sealed class CircularBuffer
+    using System;
+
+    public sealed class CircularBuffer
     {
-        private byte[] _buffer;
-        private int _head;
-        private int _tail;
+        private byte[] buffer;
+        private int head;
+        private int tail;
 
         /// <summary>
+        /// Initializes a new instance of the <see cref="CircularBuffer"/> class.
         ///     Constructs a new instance of a byte queue.
         /// </summary>
         public CircularBuffer()
         {
-            _buffer = new byte[0x10000];
+            buffer = new byte[0x10000];
         }
 
         /// <summary>
-        ///     Gets the length of the byte queue
+        ///     Gets the length of the byte queue.
         /// </summary>
         public int Length { get; private set; }
 
-        /// <summary>
-        ///     Clears the byte queue
-        /// </summary>
-        internal void Clear()
+        public byte GetID()
         {
-            _head = 0;
-            _tail = 0;
-            Length = 0;
-        }
-
-        /// <summary>
-        ///     Extends the capacity of the bytequeue
-        /// </summary>
-        private void SetCapacity(int capacity)
-        {
-            byte[] newBuffer = new byte[capacity];
-
-            if (Length > 0)
+            if (Length >= 1)
             {
-                if (_head < _tail)
-                    Buffer.BlockCopy(_buffer, _head, newBuffer, 0, Length);
-                else
-                {
-                    Buffer.BlockCopy(_buffer, _head, newBuffer, 0, _buffer.Length - _head);
-                    Buffer.BlockCopy(_buffer, 0, newBuffer, _buffer.Length - _head, _tail);
-                }
+                return buffer[head];
             }
 
-            _head = 0;
-            _tail = Length;
-            _buffer = newBuffer;
+            return 0xFF;
+        }
+
+        public int GetLength()
+        {
+            if (Length >= 3)
+            {
+                return buffer[(head + 2) % buffer.Length] | (buffer[(head + 1) % buffer.Length] << 8);
+            }
+
+            // return (_buffer[(_head + 1) % _buffer.Length] << 8) | _buffer[(_head + 2) % _buffer.Length];
+            return 0;
         }
 
         /// <summary>
@@ -83,26 +68,33 @@ namespace ClassicUO.Network
         /// <param name="buffer">Buffer to enqueue</param>
         /// <param name="offset">The zero-based byte offset in the buffer</param>
         /// <param name="size">The number of bytes to enqueue</param>
-        internal void Enqueue(byte[] buffer, int offset, int size)
+        public void Enqueue(byte[] buffer, int offset, int size)
         {
-            if (Length + size > _buffer.Length) SetCapacity((Length + size + 2047) & ~2047);
-
-            if (_head < _tail)
+            if (Length + size > this.buffer.Length)
             {
-                int rightLength = _buffer.Length - _tail;
+                SetCapacity((Length + size + 2047) & ~2047);
+            }
+
+            if (head < tail)
+            {
+                int rightLength = this.buffer.Length - tail;
 
                 if (rightLength >= size)
-                    Buffer.BlockCopy(buffer, offset, _buffer, _tail, size);
+                {
+                    Buffer.BlockCopy(buffer, offset, this.buffer, tail, size);
+                }
                 else
                 {
-                    Buffer.BlockCopy(buffer, offset, _buffer, _tail, rightLength);
-                    Buffer.BlockCopy(buffer, offset + rightLength, _buffer, 0, size - rightLength);
+                    Buffer.BlockCopy(buffer, offset, this.buffer, tail, rightLength);
+                    Buffer.BlockCopy(buffer, offset + rightLength, this.buffer, 0, size - rightLength);
                 }
             }
             else
-                Buffer.BlockCopy(buffer, offset, _buffer, _tail, size);
+            {
+                Buffer.BlockCopy(buffer, offset, this.buffer, tail, size);
+            }
 
-            _tail = (_tail + size) % _buffer.Length;
+            tail = (tail + size) % this.buffer.Length;
             Length += size;
         }
 
@@ -113,53 +105,82 @@ namespace ClassicUO.Network
         /// <param name="offset">The zero-based byte offset in the buffer</param>
         /// <param name="size">The number of bytes to dequeue</param>
         /// <returns>Number of bytes dequeued</returns>
-        internal int Dequeue(byte[] buffer, int offset, int size)
+        public int Dequeue(byte[] buffer, int offset, int size)
         {
-            if (size > Length) size = Length;
+            if (size > Length)
+            {
+                size = Length;
+            }
 
-            if (size == 0) return 0;
+            if (size == 0)
+            {
+                return 0;
+            }
 
-            if (_head < _tail)
-                Buffer.BlockCopy(_buffer, _head, buffer, offset, size);
+            if (head < tail)
+            {
+                Buffer.BlockCopy(this.buffer, head, buffer, offset, size);
+            }
             else
             {
-                int rightLength = _buffer.Length - _head;
+                int rightLength = this.buffer.Length - head;
 
                 if (rightLength >= size)
-                    Buffer.BlockCopy(_buffer, _head, buffer, offset, size);
+                {
+                    Buffer.BlockCopy(this.buffer, head, buffer, offset, size);
+                }
                 else
                 {
-                    Buffer.BlockCopy(_buffer, _head, buffer, offset, rightLength);
-                    Buffer.BlockCopy(_buffer, 0, buffer, offset + rightLength, size - rightLength);
+                    Buffer.BlockCopy(this.buffer, head, buffer, offset, rightLength);
+                    Buffer.BlockCopy(this.buffer, 0, buffer, offset + rightLength, size - rightLength);
                 }
             }
 
-            _head = (_head + size) % _buffer.Length;
+            head = (head + size) % this.buffer.Length;
             Length -= size;
 
             if (Length == 0)
             {
-                _head = 0;
-                _tail = 0;
+                head = 0;
+                tail = 0;
             }
 
             return size;
         }
 
-        public byte GetID()
+        /// <summary>
+        ///     Clears the byte queue.
+        /// </summary>
+        internal void Clear()
         {
-            if (Length >= 1) return _buffer[_head];
-
-            return 0xFF;
+            head = 0;
+            tail = 0;
+            Length = 0;
         }
 
-        public int GetLength()
+        /// <summary>
+        ///     Extends the capacity of the bytequeue.
+        /// </summary>
+        private void SetCapacity(int capacity)
         {
-            if (Length >= 3)
-                return _buffer[(_head + 2) % _buffer.Length] | (_buffer[(_head + 1) % _buffer.Length] << 8);
+            byte[] newBuffer = new byte[capacity];
 
-            // return (_buffer[(_head + 1) % _buffer.Length] << 8) | _buffer[(_head + 2) % _buffer.Length];
-            return 0;
+            if (Length > 0)
+            {
+                if (head < tail)
+                {
+                    Buffer.BlockCopy(buffer, head, newBuffer, 0, Length);
+                }
+                else
+                {
+                    Buffer.BlockCopy(buffer, head, newBuffer, 0, buffer.Length - head);
+                    Buffer.BlockCopy(buffer, 0, newBuffer, buffer.Length - head, tail);
+                }
+            }
+
+            head = 0;
+            tail = Length;
+            buffer = newBuffer;
         }
     }
 }
